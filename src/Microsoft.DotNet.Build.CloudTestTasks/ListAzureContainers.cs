7cb8af59cf4261f2cab0678357fcd72a7c9d074d
costahome <costa_christopher@hotmail.com>
2016-03-31 00:57:35 -0700

    misc changes, late at night, pushing for preservation

---
+++ src/Microsoft.DotNet.Build.CloudTestTasks/ListAzureContainers.cs
@@ -0,0 +1,50 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+
+namespace Microsoft.DotNet.Build.CloudTestTasks
+{
+    public sealed class ListAzureContainers : Task
+    {
+        /// <summary>
+        /// The Azure account name used when creating the connection string.
+        /// </summary>
+        [Required]
+        public string AccountName { get; set; }
+
+        /// <summary>
+        /// The Azure account key used when creating the connection string.
+        /// </summary>
+        [Required]
+        public string AccountKey { get; set; }
+
+        /// <summary>
+        /// Prefix of Azure containers desired to return;
+        /// </summary>
+        public string Prefix { get; set; }
+
+        /// <summary>
+        /// An item group of blob filenames to download.  
+        /// </summary>
+        [Output]
+        public ITaskItem[] ContainerNames { get; set; }
+
+        public override bool Execute()
+        {
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(string.Format("DefaultEndpointsProtocol=https;AccountName={0};AccountKey={1}", AccountName, AccountKey));
+            CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+            IEnumerable<CloudBlobContainer> containers = blobClient.ListContainers(Prefix);
+            ContainerNames = containers.Select(c => new TaskItem(c.Name)).ToArray();
+            return true;
+        }
+    }
+}
---
+++ src/Microsoft.DotNet.Build.Tasks/GetPackageNumberFromPackageDrop.cs
@@ -0,0 +1,49 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Linq;
+using System.IO;
+using System.Text.RegularExpressions;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+namespace Microsoft.DotNet.Build.Tasks
+{
+    public sealed class GetPackageNumberFromPackageDrop : Task
+    {
+        [Required]
+        public string[] PackageDrops { get; set; }
+
+        [Output]
+        public string PackageNumber { get; set; }
+
+        public override bool Execute()
+        {
+            foreach (string packageDrop in PackageDrops)
+            {
+                if (!Directory.Exists(packageDrop))
+                {
+                    continue;
+                }
+                string[] files = Directory.GetFiles(packageDrop);
+                if (files == null || files.Count() == 0)
+                {
+                    continue;
+                }
+
+                Regex packageMatch = new Regex(@"[^-]+-(\d\d\d\d\d)-");
+                foreach (string file in files)
+                {
+                    Match m = packageMatch.Match(file);
+                    if (m.Success)
+                    {
+                        PackageNumber = m.Groups[1].Value;
+                        return true;
+                    }
+                }
+            }
+            return true;
+        }
+    }
+}
