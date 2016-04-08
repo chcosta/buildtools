e4e1bc4e601c8fba34baee0b321ca0718b988178
Mariana Rios Flores <mariari@microsoft.com>
2016-04-08 14:19:46 -0700

    Port changes to run in Core

--- src/Microsoft.DotNet.Build.CloudTestTasks/ListAzureContainers.cs
+++ src/Microsoft.DotNet.Build.CloudTestTasks/ListAzureContainers.cs
@@ -2,14 +2,17 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.IO;
 using Microsoft.Build.Framework;
 using Microsoft.Build.Utilities;
-using Microsoft.WindowsAzure.Storage;
-using Microsoft.WindowsAzure.Storage.Blob;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Xml;
+
+using Task = Microsoft.Build.Utilities.Task;
 
 namespace Microsoft.DotNet.Build.CloudTestTasks
 {
@@ -28,9 +31,9 @@ namespace Microsoft.DotNet.Build.CloudTestTasks
         public string AccountKey { get; set; }
 
         /// <summary>
-        /// Prefix of Azure containers desired to return;
-        /// </summary>
-        public string Prefix { get; set; }
+         /// Prefix of Azure containers desired to return;
+         /// </summary>
+         public string Prefix { get; set; }
 
         /// <summary>
         /// An item group of blob filenames to download.  
@@ -40,10 +43,44 @@ namespace Microsoft.DotNet.Build.CloudTestTasks
 
         public override bool Execute()
         {
-            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(string.Format("DefaultEndpointsProtocol=https;AccountName={0};AccountKey={1}", AccountName, AccountKey));
-            CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
-            IEnumerable<CloudBlobContainer> containers = blobClient.ListContainers(Prefix);
-            ContainerNames = containers.Select(c => new TaskItem(c.Name)).ToArray();
+            return ExecuteAsync().GetAwaiter().GetResult();
+        }
+
+        public async Task<bool> ExecuteAsync()
+        {
+            Log.LogMessage(MessageImportance.High, "List of Azure containers in storage account '{0}'.", AccountName);
+
+            DateTime dateTime = DateTime.UtcNow;
+            string url = string.Format("https://{0}.blob.core.windows.net/?comp=list", AccountName);
+            
+            Log.LogMessage(MessageImportance.Normal, "Sending request to list containers in account '{0}'.", AccountName);
+
+            using (HttpClient client = new HttpClient())
+            {
+                using (HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, url))
+                {
+                    request.Headers.Add(AzureHelper.DateHeaderString, dateTime.ToString("R", CultureInfo.InvariantCulture));
+                    request.Headers.Add(AzureHelper.VersionHeaderString, AzureHelper.StorageApiVersion);
+                    request.Headers.Add(AzureHelper.AuthorizationHeaderString, AzureHelper.AuthorizationHeader(
+                            AccountName,
+                            AccountKey,
+                            "GET",
+                            dateTime,
+                            request));
+
+                    XmlDocument responseFile;
+                    using (HttpResponseMessage response = await client.SendAsync(request))
+                    {
+                        responseFile = new XmlDocument();
+                        responseFile.LoadXml(await response.Content.ReadAsStringAsync());
+                        XmlNodeList elemList = responseFile.GetElementsByTagName("Name");
+
+                        ContainerNames = (from x in elemList.Cast<XmlNode>()
+                                          where x.InnerText.Contains(Prefix)
+                                          select new TaskItem(x.InnerText)).ToArray();
+                    }
+                }
+            }
             return true;
         }
     }
